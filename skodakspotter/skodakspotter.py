f79bede2139c3378155b4458026e8b85cd6d147e
janbern <45913510+janbern@users.noreply.github.com>
2022-02-17 02:26:38 +0100

    Update skodakspotter.py

--- skodakspotter/skodakspotter.py
+++ skodakspotter/skodakspotter.py
@@ -1,24 +1,22 @@
 from redbot.core import commands
 
-class skodakspotter(commands.Cog):
+
+class SkodakSpotter(commands.Cog):
     """Je Stegomrd online?"""
 
     def __init__(self, bot):
-	self.bot = bot
+        self.bot = bot
 
     @commands.command()
     async def skodak(self, ctx):
 
         import requests
         from bs4 import BeautifulSoup
-		
-
-        URL = 'http://armory.twinstar.cz/character-feed.xml?r=Apollo&cn=Stegomrd'
-        page = requests.get(URL)
+        url = 'http://armory.twinstar.cz/character-feed.xml?r=Apollo&cn=Stegomrd'
+        page = requests.get(url)
         soup = BeautifulSoup(page.text, 'xml')
         timestamp = soup.find('character')
-        
-	if timestap != None:
-		await ctx.send(timestamp['lastModified'])
-	else:
-		await ctx.send("Sorry, can't connect to Twinstar rn")
+        if timestamp is not None:
+            await ctx.send(timestamp['lastModified'])
+        else:
+            await ctx.send("Sorry, can't connect to Twinstar rn")
